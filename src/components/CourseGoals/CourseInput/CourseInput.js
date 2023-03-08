9f9ca1c00007459412b65389fb14336821823962
Arek Lechnik <alechnik@gmail.com>
2023-03-08 06:42:52 +0100

    Adding validation to input + dynamic CSS

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,18 +6,28 @@ import "./CourseInput.css";
 
 const CoursInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
+	const [isValid, setIsValid] = useState(true);
 
 	const handleGoalInputChange = (event) => {
+		if (event.target.value.trim().length > 0) {
+			setIsValid(true);
+		}
 		setEnteredValue(event.target.value);
 	};
 
 	const handleFormeSubmit = (event) => {
 		event.preventDefault();
+
+		if (enteredValue.trim().length === 0) {
+			setIsValid(false);
+			return;
+		}
+
 		props.onAddGoal(enteredValue);
 	};
 	return (
 		<form onSubmit={handleFormeSubmit}>
-			<div className='form-control'>
+			<div className={`form-control ${!isValid ? "invalid" : ""}`}>
 				<label>Course Goal</label>
 				<input type='text' onChange={handleGoalInputChange} />
 				<Button type='Submit'>Add Goal</Button>
